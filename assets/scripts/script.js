2ed3355df2c8f90570fd6c8cfbcd61eda9ea2db3
Darren Sarra <adriansarra18@gmail.com>
2020-02-14 18:33:22 -0500

    Add into options array password options that would be included in the password

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -9,6 +9,7 @@ let lowercaseLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k",
 let numericValues = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 let specialCharacter = ["@", "%", "+", "\\", "/", "'", "!", "#", "$", "^",
 "?", ":", ",", ")", "(", "}", "{", "]", "[", "~", "-", "_", "."];
+let options = [];
 
 // Write password to the #password input
 function writePassword() {
@@ -29,9 +30,62 @@ function generatePassword(){
     let passLenght = prompt("How many characters would you like your password to contain?");
     console.log(passLenght);
 
+    // Dealing with special characters
     let useSpecialChar = confirm("Click OK to confirm including special characters.");
+
+    if (useSpecialChar)
+    {
+        console.log("Yes, use special char");
+        options.push(specialCharacter);
+    }
+    else
+    {
+        console.log("No, don't use special char");
+    }
+
+
+    // Dealing with numeric values
     let useNumericVal = confirm("Click OK to confirm including numeric characters.");
+
+    if (useNumericVal)
+    {
+        console.log("Yes, use numeric val");
+        options.push(numericValues);
+    }
+    else
+    {
+        console.log("No, don't use numeric val");
+    }
+
+    // Dealing with lowercases
     let useLowercase = confirm("Click OK to confirm including lowercase characters.");
+
+    if (useLowercase)
+    {
+        console.log("Yes, use lowercase letters");
+        options.push(lowercaseLetters);
+    }
+    else
+    {
+        console.log("No, don't use lowercase letters");
+    }
+
+    // Dealing with uppercases
     let useUppercase = confirm("Click OK to confirm including uppercase characters.");
+
+    if (useUppercase)
+    {
+        console.log("Yes, use uppercase letters");
+        options.push(uppercaseLetters);
+    }
+    else
+    {
+        console.log("No, don't use uppercase letters");
+    }
+
+
+    
+
+
 
 }
